17c5ab67b797daf8fbfaf931c70390e224053046
ahmedherhesh <ahmedherhesh3@gmail.com>
2022-08-07 17:41:21 +0200

    some work

--- react-e-commerce/src/views/cart.jsx
+++ react-e-commerce/src/views/cart.jsx
@@ -1,35 +1,55 @@
 import React, { Component } from 'react';
-import '../assets/css/cart.css';
 import moto from '../moto.jpg';
-import { FaAccusoft } from 'react-icons/fa';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {Link} from "react-router-dom";
+import { FaPlus, FaMinus, FaTrashAlt } from 'react-icons/fa';
 
 class Cart extends Component {
     state = {}
     render() {
         return (
             <div className='container'>
-                <div className="row" style={{ flexWrap: 'no-wrap' }}>
-                    <div className="product row" style={{ flexWrap: 'no-wrap', backgroundColor: '#fff', flex: 3, justifyContent: 'space-between' }}>
-                        <div className='row col' style={{ flex: 3 }}>
-                            <img className='img ' src={moto} alt="" />
-                            <h3 className="title col">
-                                Samsung Galaxy A52 - 6.5 256GB/8GB Dual Sim Mobile Phone - Awesome White
-                            </h3>
-                        </div>
-                        <div className="price-section col" style={{ flex: 1, textAlign: 'right' }}>
-                            <p className="price">EGP 8,490.00</p>
-                            <p className="old-price">EGP 8,990.00</p><span className='discount'> -6%</span>
-                        </div>
-                        <div className="row" style={{ flexWrap: 'no-wrap' ,justifyContent:'space-between'}}>
-                            <span className="remove-btn col btn btn-danger"> <FontAwesomeIcon icon={['fab', 'apple']} />Remove</span>
-                            <div className="counter col" style={{ textAlign: 'right' }}>
-                                <span className="decrement btn btn-info">-</span>
-                                <span className="increment btn btn-info m-1">+</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div style={{ flex: 1 }}></div>
+                <div className="row">
+
+                    {
+                        [0, 1, 2].map((d, i) => {
+
+                            return (
+                                <React.Fragment>
+
+                                    <div className="product row col-9 rounded bg-white justify-content-around p-0 m-1">
+                                        <div className='row col-9'>
+                                            <img className='img col-1' src={moto} alt="" />
+                                            <h3 className="title col">
+                                                Samsung Galaxy A52 - 6.5 256GB/8GB Dual Sim Mobile Phone - Awesome White
+                                            </h3>
+                                        </div>
+                                        <div className="price-section col-3 text-end">
+                                            <p className="price">EGP 8,490.00</p>
+                                            <p className="old-price">EGP 8,990.00</p><span className='discount'> -6%</span>
+                                        </div>
+                                        <div className="row justify-content-between p-2 p-r-0">
+                                            <span className="trash-btn btn col-2 text-start"> <FaTrashAlt /> Remove</span>
+                                            <div className="counter col-2 text-end">
+                                                <span className="decrement"><FaMinus /></span>
+                                                <span className="qty m-2">1</span>
+                                                <span className="increment m-1"><FaPlus /></span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    {i === 0 ?
+                                        <div className='col-2 bg-white p-2 rounded'>
+                                            <h4>Cost</h4>
+                                            <hr />
+                                            <span> Total : EGP 8,490.00 </span>
+                                            <Link to="/register" className="btn btn-secondary d-block m-0">Checkout</Link>
+                                        </div>
+                                    : ''}
+
+                                </React.Fragment>
+
+                            )
+                        })
+                    }
                 </div>
             </div>
         )
