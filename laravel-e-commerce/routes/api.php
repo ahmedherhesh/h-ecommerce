8af5e0c378178c69a9f8e730fb726b0491841d8a
ahmedherhesh <ahmedherhesh3@gmail.com>
2022-08-15 20:30:52 +0200

    some work

--- laravel-e-commerce/routes/api.php
+++ laravel-e-commerce/routes/api.php
@@ -7,20 +7,6 @@ use App\Http\Resources\API\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-// header('Access-Control-Allow-Origin', 'http://localhost:8000/');
-// header('Access-Control-Allow-Credentials', true);
-// header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-// header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return new UserResource(auth('sanctum')->user());
 });
