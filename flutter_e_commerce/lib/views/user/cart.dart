00c2393a8a2153e02a69239b97b331e9a31f375c
ahmedherhesh <ahmedherhesh3@gmail.com>
2022-09-26 19:37:42 +0200

    shopping cart

--- flutter_e_commerce/lib/views/user/cart.dart
+++ flutter_e_commerce/lib/views/user/cart.dart
@@ -1,13 +1,261 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_e_commerce/design_settings/values.dart';
 import 'package:flutter_e_commerce/views/components/functions.dart';
 
 class Cart extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: Functions.appBar(context: context),
-      body: Center(
-        child: Text('Cart'),
-      ),
+      body: ListView(children: [
+        SizedBox(height: 10),
+        ...List.generate(
+          3,
+          (index) => Container(
+            margin: EdgeInsets.only(left: 15, top: 10, right: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    //Cart Image
+                    Container(
+                      clipBehavior: Clip.hardEdge,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          boxShadow: [
+                            BoxShadow(
+                              blurRadius: 4,
+                              color: shadowColor,
+                            )
+                          ]),
+                      child: Image.asset(
+                        'assets/images/sale.jpg',
+                        width: 80,
+                        height: 80,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    //Cart Title, Price
+                    Container(
+                      margin: EdgeInsets.only(left: 10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Herhesh Product',
+                            style: TextStyle(
+                              color: textColor,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            '\$19.99',
+                            style: TextStyle(
+                              color: textColor,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    SizedBox(
+                      height: 30,
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.close_rounded,
+                          size: 18,
+                          color: textColor,
+                        ),
+                      ),
+                    ),
+                    //cart increment or decrement
+                    Row(
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            color: primaryColor,
+                            Icons.remove_circle,
+                            size: 28,
+                          ),
+                        ),
+                        Text('1'),
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.add_circle,
+                            color: primaryColor,
+                            size: 28,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.all(15),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 6,
+                color: shadowColor,
+              ),
+            ],
+            color: Colors.white,
+          ),
+          child: Form(
+              child: Stack(
+            children: [
+              TextFormField(
+                autofocus: false,
+                cursorColor: primaryColor,
+                decoration: InputDecoration(
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  hintText: 'Promo Code',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25.0),
+                      borderSide: BorderSide.none),
+                ),
+              ),
+              Positioned(
+                top: 8,
+                right: 8,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 10,
+                        color: Color.fromARGB(255, 199, 208, 212),
+                      ),
+                    ],
+                    gradient: LinearGradient(
+                      begin: Alignment.centerRight,
+                      end: Alignment.centerLeft,
+                      colors: [
+                        Color.fromARGB(255, 14, 163, 156),
+                        primaryColor,
+                      ],
+                    ),
+                  ),
+                  clipBehavior: Clip.hardEdge,
+                  child: MaterialButton(
+                    minWidth: 100,
+                    padding: EdgeInsets.all(8),
+                    onPressed: () {},
+                    child: Text(
+                      'Apply',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          )),
+        ),
+        Container(
+          padding: EdgeInsets.all(15),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Subtotal',
+                    style: TextStyle(
+                      color: textColor,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    '\$59.97',
+                    style: TextStyle(
+                      color: textColor,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10, bottom: 10),
+                padding: EdgeInsets.only(top: 10, bottom: 10),
+                decoration: BoxDecoration(
+                  border: Border(
+                    top: BorderSide(color: primaryColor, width: 1),
+                    bottom: BorderSide(color: primaryColor, width: 1),
+                  ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Shipping',
+                      style: TextStyle(
+                        color: textColor,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      '\$4.99',
+                      style: TextStyle(
+                        color: textColor,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Total',
+                    style: TextStyle(
+                      color: textColor,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    '\$64.95',
+                    style: TextStyle(
+                      color: textColor,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        )
+      ]),
     );
   }
 }
