71d9050e6e47c18bc91ce4cb99fd85221dcdde02
ahmedherhesh <ahmedherhesh3@gmail.com>
2022-09-26 17:18:08 +0200

    product page

--- flutter_e_commerce/lib/design_settings/values.dart
+++ flutter_e_commerce/lib/design_settings/values.dart
@@ -2,3 +2,5 @@ import 'package:flutter/material.dart';
 //colors
 
 Color primaryColor = Color.fromARGB(255, 3, 179, 179);
+Color textColor    = Colors.blueGrey;
+Color shadowColor    = Colors.grey;
--- flutter_e_commerce/lib/views/user/product.dart
+++ flutter_e_commerce/lib/views/user/product.dart
@@ -12,149 +12,260 @@ class Product extends StatefulWidget {
 }
 
 class _ProductState extends State<Product> {
+  int currentSlide = 0;
   @override
   Widget build(BuildContext context) {
     final args = ModalRoute.of(context)!.settings.arguments as Product;
     return Scaffold(
-        appBar: Functions.appBar(),
-        bottomNavigationBar: Container(
-          padding: EdgeInsets.only(top: 8, right: 15, bottom: 5, left: 15),
-          decoration: BoxDecoration(
-            // gradient: LinearGradient(
-            //   begin: Alignment.centerRight,
-            //   end: Alignment.centerLeft,
-            //   colors: [
-            //     primaryColor,
-            //     Color.fromARGB(255, 24, 160, 153),
-            //   ],
-            // ),
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(30),
-              topRight: Radius.circular(30),
+      extendBody: true,
+      appBar: Functions.appBar(),
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.only(top: 8, right: 15, bottom: 5, left: 15),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          ),
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 6,
+              color: Color(0xffdddddd),
+            )
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              '\$19.99',
+              style: TextStyle(
+                color: primaryColor,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-            boxShadow: [
-              BoxShadow(
-                blurRadius: 6,
-                color: Color(0xffdddddd),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 10,
+                    color: Color.fromARGB(255, 199, 208, 212),
+                  ),
+                ],
+                gradient: LinearGradient(
+                  begin: Alignment.centerRight,
+                  end: Alignment.centerLeft,
+                  colors: [
+                    Color.fromARGB(255, 14, 163, 156),
+                    primaryColor,
+                  ],
+                ),
+              ),
+              clipBehavior: Clip.hardEdge,
+              child: MaterialButton(
+                minWidth: 100,
+                padding: EdgeInsets.all(8),
+                onPressed: () {},
+                child: Row(
+                  children: [
+                    Text(
+                      'Cart',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(width: 5),
+                    Icon(
+                      Icons.shopping_cart,
+                      color: Colors.white.withOpacity(.7),
+                      size: 25,
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: ListView(
+        children: [
+          //Product Page Slider
+          Column(
+            children: [
+              Container(
+                clipBehavior: Clip.hardEdge,
+                height: 200,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey,
+                        blurRadius: 6,
+                        spreadRadius: 2,
+                      ),
+                    ]),
+                margin: EdgeInsets.all(10),
+                child: PageView(
+                  onPageChanged: (index) => setState(() {
+                    currentSlide = index;
+                  }),
+                  children: List.generate(
+                    4,
+                    (index) => Image.asset(
+                      fit: BoxFit.cover,
+                      'assets/images/sale.jpg',
+                    ),
+                  ),
+                ),
+              ),
+              //Slider Indicators
+              Container(
+                alignment: Alignment.center,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisSize: MainAxisSize.min,
+                  children: List.generate(
+                    4,
+                    (index) => Container(
+                      margin: const EdgeInsets.only(right: 5),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(6),
+                        color: currentSlide == index ? primaryColor : textColor,
+                      ),
+                      width: currentSlide == index ? 14 : 6,
+                      height: 6,
+                    ),
+                  ),
+                ),
               )
             ],
           ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                '\$19.99',
-                style: TextStyle(
-                  color: primaryColor,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
+          Container(
+            margin: const EdgeInsets.only(top: 10, right: 10, left: 10),
+            padding:
+                const EdgeInsets.only(top: 5, bottom: 5, left: 10, right: 10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
               ),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  boxShadow: [
-                    BoxShadow(
-                      blurRadius: 10,
-                      color: Color.fromARGB(255, 199, 208, 212),
-                    ),
-                  ],
-                  gradient: LinearGradient(
-                    begin: Alignment.centerRight,
-                    end: Alignment.centerLeft,
-                    colors: [
-                      Color.fromARGB(255, 14, 163, 156),
-                      primaryColor,
-                    ],
-                  ),
-                ),
-                clipBehavior: Clip.hardEdge,
-                child: MaterialButton(
-                  minWidth: 100,
-                  padding: EdgeInsets.all(8),
-                  onPressed: () {},
-                  child: Row(
-                    children: [
-                      Text(
-                        'Cart',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(width: 5),
-                      Icon(
-                        Icons.shopping_cart,
-                        color: Colors.white.withOpacity(.7),
-                        size: 25,
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-        body: ListView(
-          children: [
-            //Product Page Slider
-            Column(
+              boxShadow: [
+                BoxShadow(blurRadius: 6, color: shadowColor),
+              ],
+            ),
+            //title, rating
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              // crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Container(
-                  clipBehavior: Clip.hardEdge,
-                  height: 200,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Color(0xFFdddddd),
-                          blurRadius: 6,
-                          spreadRadius: 2,
-                        ),
-                      ]),
-                  margin: EdgeInsets.all(10),
-                  child: PageView(
-                    children: List.generate(
-                      4,
-                      (index) => Image.asset(
-                        fit: BoxFit.cover,
-                        'assets/images/sale.jpg',
-                      ),
-                    ),
-                  ),
-                ),
-                //Slider Indicators
-                Container(
-                  width: 50,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      CircleAvatar(
-                        backgroundColor: primaryColor,
-                        radius: 4,
+                      Text(
+                        'Herhesh Product',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: textColor,
+                        ),
                       ),
-                      CircleAvatar(
-                        backgroundColor: Colors.blueGrey,
-                        radius: 4,
-                      ),
-                      CircleAvatar(
-                        backgroundColor: Colors.blueGrey,
-                        radius: 4,
-                      ),
-                      CircleAvatar(
-                        backgroundColor: Colors.blueGrey,
-                        radius: 4,
+                      Text(
+                        'Ahmed Herhesh',
+                        style: TextStyle(fontSize: 14, color: textColor),
                       ),
                     ],
                   ),
+                ),
+                //rating
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          '4.5',
+                          style: TextStyle(color: textColor),
+                        ),
+                        Icon(
+                          Icons.star_outline_outlined,
+                          color: textColor,
+                        ),
+                      ],
+                    ),
+                    Container(
+                      // margin: EdgeInsets.only(right: 10),
+                      child: Text(
+                        '(135 Reviews)',
+                        style: TextStyle(color: textColor),
+                      ),
+                    ),
+                  ],
                 )
               ],
             ),
-          ],
-        ));
+          ),
+          DefaultTabController(
+            length: 2,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(blurRadius: 6, color: shadowColor),
+                ],
+              ),
+              padding: const EdgeInsets.all(10),
+              margin: const EdgeInsets.only(top: 10, right: 10, left: 10),
+              child: Column(
+                children: [
+                  Container(
+                    child: TabBar(
+                        padding: EdgeInsets.only(bottom: 15),
+                        labelColor: primaryColor,
+                        unselectedLabelColor: textColor,
+                        indicatorColor: primaryColor,
+                        tabs: [
+                          Text(
+                            'Details',
+                            style: TextStyle(
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            'Reviews(135)',
+                            style: TextStyle(
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ]),
+                  ),
+                  Container(
+                    height: 200,
+                    child: TabBarView(children: [
+                      Text(
+                        '* Details Details Details Details Details \n* Details Details Details\n* Details Details Details Details\n* Details Details Details Details Details Details\n* Details Details Details Details Details Details\n* Details Details Details\n* Details Details Details',
+                        style: TextStyle(fontSize: 16, color: textColor),
+                      ),
+                      Text(
+                        'hello hello hello hello\nhello hello hello hello\nhello hello hello hello\nhello hello hello hello\nhello hello hello hello\nhello hello hello hello\nhello hello hello hello',
+                        style: TextStyle(fontSize: 16, color: textColor),
+                      ),
+                    ]),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
