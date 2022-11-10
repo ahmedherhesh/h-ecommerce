2cb53465bfb2c37282f0524448dba2dfb7ce2996
ahmedherhesh <ahmedherhesh3@gmail.com>
2022-11-10 02:58:13 +0200

    owner

--- laravel-e-commerce/routes/console.php
+++ laravel-e-commerce/routes/console.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('owner',function(){
+    $this->comment('Name : Ahmed Herhesh');
+    $this->comment('Phone : 01152958015');
+});
