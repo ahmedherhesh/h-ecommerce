176de5f7363afab451e1299b04b20b423062c8d2
ahmedherhesh <ahmedherhesh3@gmail.com>
2022-11-25 16:52:55 +0200

    run webview

--- flutter_e_commerce/lib/views/user/payments/paypal.dart
+++ flutter_e_commerce/lib/views/user/payments/paypal.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_e_commerce/design_settings/values.dart';
 import 'package:flutter_e_commerce/helpers/functions.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class PayPal extends StatefulWidget {
   State<PayPal> createState() => _PayPalState();
 }
 
 class _PayPalState extends State<PayPal> {
-  void initState() {
-    super.initState();
-  }
-
-  final url = "https://www.google.com";
-
+  bool loading = true;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: appBar(context: context, title: 'Checkout')
-        // body: WebView(
-        //   initialUrl: 'https://flutter.dev',
-        // ),
-        );
+    return Scaffold(
+      appBar: appBar(context: context, title: 'Checkout'),
+      body: Stack(
+        children: [
+          WebView(
+            initialUrl: 'https://malik2.com/payment?plan_id=2&app_token=app_token',
+            javascriptMode: JavascriptMode.unrestricted,
+            onPageFinished: (url) => setState(() => loading = false),
+          ),
+          loading ? Center(child: CircularProgressIndicator(color: primaryColor)) : const SizedBox()
+        ],
+      ),
+    );
   }
 }
