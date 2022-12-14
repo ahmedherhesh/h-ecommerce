12c62ae825ea99a3aeccd6c59d9530c29aa78d90
ahmedherhesh <ahmedherhesh3@gmail.com>
2022-12-14 22:46:12 +0200

    static domain

--- flutter_e_commerce/lib/init.dart
+++ flutter_e_commerce/lib/init.dart
@@ -1,5 +1,5 @@
 Map initData = {
-  'apiUrl': 'http://197.63.26.113/h-ecommerce/laravel-e-commerce/public/api/v1',
+  'apiUrl': 'https://herhesh.tech/h-ecommerce/laravel-e-commerce/public/api/v1',
   'user': '',
   'headers': {'Authorization': ''},
   'Authorized': false
