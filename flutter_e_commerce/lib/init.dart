11a93e86eb4c6fad6d982800db50a359dfd93e75
ahmedherhesh <ahmedherhesh3@gmail.com>
2023-01-07 22:27:59 +0200

    herhesh.tech

--- flutter_e_commerce/lib/init.dart
+++ flutter_e_commerce/lib/init.dart
@@ -1,7 +1,7 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 Map initData = {
-  'apiUrl': 'http://192.168.1.180/h-ecommerce/laravel-e-commerce/public/api/v1',
+  'apiUrl': 'https://herhesh.tech/h-ecommerce/laravel-e-commerce/public/api/v1',
   'user': '',
   'headers': {'Authorization': ''},
   'Authorized': false
