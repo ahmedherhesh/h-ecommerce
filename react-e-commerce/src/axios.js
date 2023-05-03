012094352eb07a865e426996ea97878dfad06844
ahmedherhesh <ahmedherhesh3@gmail.com>
2023-05-03 12:35:23 +0300

    remove react

--- react-e-commerce/src/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default  axios.create({
-    baseURL : 'http://192.168.1.3/h-ecommerce/laravel-e-commerce/public/api/v1/',
-    headers : {
-        'Authorization'    : 'Bearer ' + localStorage.getItem('token')
-    },
-});
